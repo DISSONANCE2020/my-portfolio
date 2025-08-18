7e38cae4f71d1aa4c8dc8ac9cf9f4bfd20b0d029
Jose Gabriel Cruz <gabrielbuenaventura2020@gmail.com>
2025-08-18 11:04:47 +0800

    Hero Section De-Hardcode

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,44 +1,7 @@
 import styles from "./HeroSection.module.css";
 import myPhoto from "../../assets/images/HeroSection/myPhoto.png";
 
-import reactLogo from "../../assets/logos/reactLogo.png";
-import laravelLogo from "../../assets/logos/laravelLogo.png";
-import jsLogo from "../../assets/logos/jsLogo.png";
-import phpLogo from "../../assets/logos/phpLogo.png";
-import nodeLogo from "../../assets/logos/nodeLogo.png";
-import xamppLogo from "../../assets/logos/xamppLogo.png";
-import mysqlLogo from "../../assets/logos/mysqlLogo.png";
-import githubLogo from "../../assets/logos/githubLogo.png";
-import postmanLogo from "../../assets/logos/postmanLogo.png";
-import figmaLogo from "../../assets/logos/figmaLogo.png";
-import tailwindLogo from "../../assets/logos/tailwindLogo.png";
-import mantineLogo from "../../assets/logos/mantineLogo.svg";
-
-const gridCells = [
-  // Row 1
-  { logo: reactLogo, text: "React", href: "https://react.dev/" },
-  { logo: laravelLogo, text: "Laravel 11", href: "https://laravel.com/" },
-  {
-    logo: jsLogo,
-    text: "JavaScript",
-    href: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-  },
-
-  // Row 2
-  { logo: phpLogo, text: "PHP", href: "https://www.php.net/docs.php" },
-  { logo: nodeLogo, text: "Node.js", href: "https://nodejs.org/en" },
-  { logo: xamppLogo, text: "XAMPP", href: "https://www.apachefriends.org/" },
-
-  // Row 3
-  { logo: mysqlLogo, text: "MySQL", href: "https://www.mysql.com/" },
-  { logo: githubLogo, text: "GitHub", href: "https://github.com/" },
-  { logo: postmanLogo, text: "Postman", href: "https://www.postman.com/" },
-
-  // Row 4
-  { logo: figmaLogo, text: "Figma", href: "https://www.figma.com/" },
-  { logo: tailwindLogo, text: "Tailwind", href: "https://tailwindcss.com/" },
-  { logo: mantineLogo, text: "Mantine", href: "https://mantine.dev/" },
-];
+import {gridCells} from "./heroContent";
 
 const handleCellClick = (e: React.MouseEvent<HTMLDivElement>, href: string) => {
   if (e.ctrlKey || e.metaKey || e.shiftKey || e.altKey || e.button !== 0) {
