c6ac551ad2d32507065cde8b3cace9282f544900
Jose Gabriel Cruz <gabrielbuenaventura2020@gmail.com>
2025-08-18 09:21:51 +0800

    Header Bar De-Hardcode

--- src/components/HeaderBar/HeaderBar.tsx
+++ src/components/HeaderBar/HeaderBar.tsx
@@ -1,21 +1,18 @@
 import React from "react";
 import styles from "./HeaderBar.module.css";
-
-const navItems = [
-  { label: "Experience", href: "experience" },
-  { label: "Projects", href: "projects" },
-  { label: "Contact", href: "contact" },
-]
+import { navItems } from "./headerContent";
 
 export default function HeaderBar() {
-
-const handleNavClick = (e: React.MouseEvent<HTMLAnchorElement>, href: string) => {
-  e.preventDefault();
-  const section = document.getElementById(href);
-  if (section) {
-    section.scrollIntoView({ behavior: "smooth" })
-  }
-};
+  const handleNavClick = (
+    e: React.MouseEvent<HTMLAnchorElement>,
+    href: string
+  ) => {
+    e.preventDefault();
+    const section = document.getElementById(href);
+    if (section) {
+      section.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
     <header className={styles.headerBar}>
@@ -27,11 +24,22 @@ const handleNavClick = (e: React.MouseEvent<HTMLAnchorElement>, href: string) =>
         </div>
         {/* {Right Side} */}
         <nav className={styles.nav}>
-          <ul style={{ display: "flex", gap: "1rem", margin: 0, padding: 0, listStyle: "none" }}>
+          <ul
+            style={{
+              display: "flex",
+              gap: "1rem",
+              margin: 0,
+              padding: 0,
+              listStyle: "none",
+            }}
+          >
             {navItems.map((item) => (
               <li key={item.label}>
-                <a className={styles.navButton} href={`#${item.href}`}
-                onClick={(e) => handleNavClick(e, item.href)}>
+                <a
+                  className={styles.navButton}
+                  href={`#${item.href}`}
+                  onClick={(e) => handleNavClick(e, item.href)}
+                >
                   {item.label}
                 </a>
               </li>
---
+++ src/components/HeaderBar/headerContent.ts
@@ -0,0 +1,5 @@
+export const navItems = [
+  { label: "Experience", href: "experience" },
+  { label: "Projects", href: "projects" },
+  { label: "Contact", href: "contact" },
+]
